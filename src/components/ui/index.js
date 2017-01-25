16a148456f363ab0df163584091cd00b9984080f
lanccj <65615978as>
2017-01-25 17:43:40 +0800

    Initial commit

---
+++ src/components/ui/index.js
@@ -0,0 +1,17 @@
+/**
+ * UI Elements
+ *
+ * React Native Starter App
+ * https://github.com/mcnamee/react-native-starter-app
+ */
+
+import Alerts from './Alerts';
+import Button from './Button';
+import Card from './Card';
+import Spacer from './Spacer';
+import TabIcon from './TabIcon';
+import Text from './Text';
+import List from './List';
+import ListItem from './ListItem';
+
+export { Alerts, Button, Card, Spacer, TabIcon, Text, List, ListItem };
